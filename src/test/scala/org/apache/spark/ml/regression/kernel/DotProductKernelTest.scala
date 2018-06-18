003916b74abec5b4802c49953a966453cf7b8a95
Paul Tune <paul.t@canva.com>
2018-06-18 22:37:04 +1000

    Added another test

--- src/test/scala/org/apache/spark/ml/regression/kernel/DotProductKernelTest.scala
+++ src/test/scala/org/apache/spark/ml/regression/kernel/DotProductKernelTest.scala
@@ -1,6 +1,6 @@
 package org.apache.spark.ml.regression.kernel
 
-import breeze.linalg.{all, DenseMatrix => BDM, DenseVector => BDV}
+import breeze.linalg.{DenseMatrix => BDM, all}
 import breeze.numerics.abs
 import org.apache.spark.ml.linalg.Vectors
 import org.scalatest.FunSuite
@@ -8,6 +8,20 @@ import org.scalatest.FunSuite
 class DotProductKernelTest extends FunSuite {
   private val dataset =
     Array(Array(1d, 2d), Array(2d, 3d), Array(5d, 7d)).map(Vectors.dense)
+
+  test(
+    "being called after `setTrainingVector`," +
+      " `trainingKernel` should return the correct kernel matrix") {
+    val dot = new DotProductKernel(0.2)
+    dot.setTrainingVectors(dataset)
+
+    val correctKernelMatrix =
+      BDM((5.040000e+00, 8.040000e+00, 19.040000e+00),
+          (8.040000e+00, 13.040000e+00, 31.040000e+00),
+          (19.040000e+00, 31.040000e+00, 74.040000e+00))
+
+    assert(all(abs(dot.trainingKernel() - correctKernelMatrix) <:< 1e-4))
+  }
 
   private def computationalDerivative(sigma: Double, h: Double): BDM[Double] = {
     val left = new DotProductKernel(sigma - h)
